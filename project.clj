e0ef2c411469126f48576140a94926c0fded607a
Tamas Szabo <szabtam@gmail.com>
2015-08-22 10:37:16 +0300

    Upgraded libs, re-compiled.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 [figwheel "0.1.3-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48"]
+                 [figwheel "0.3.7"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-figwheel "0.1.3-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-figwheel "0.3.7"]]
 
   :cljsbuild {
     :builds [{:source-paths ["src"]
